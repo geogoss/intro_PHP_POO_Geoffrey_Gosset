6a22f5211347c8ab7e8b8b0e2bfe54dedf778a06
geoffrey <geoffreygosset123@gmail.com>
2022-03-05 01:01:24 +0100

    GG boucle doWhile pour faire le combat

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}} ">
     <title>Document</title>
 </head>
-<body>
+<body class="container bg-dark text-white">
     @yield('content')
+
+    <script src="{{asset('js/app.js')}} "></script>
 </body>
 </html>
--- resources/views/layouts/personnage.blade.php
+++ resources/views/layouts/personnage.blade.php
@@ -1,70 +1,85 @@
 <?php
 
-class Personnage {
+class Personnage
+{
     // Attributs
     private $_force;
     private $_classe = 'plombier';
     private $_couleurCasquette = 'Rouge';
     private $_vie = 100;
-    private $_nom = "unknow";
-
-
+    private $_nom = 'unknow';
 
     // constructeur
-    public function __construct($nom, $force, $couleur) {
+    public function __construct($nom, $force, $couleur)
+    {
         $this->_nom = $nom;
         $this->setForce($force);
         $this->setCouleurCasquette($couleur);
     }
 
-    // Methode -> simple fonction
-    public function getForce () {
+    // Méthodes -> simples fonctions
+    public function getForce()
+    {
         return $this->_force;
     }
-    public function setForce ($force) {
+    public function setForce($force)
+    {
         $this->_force = $force;
     }
-    public function getCouleurCasquette () {
+    public function getCouleurCasquette()
+    {
         return $this->_couleurCasquette;
     }
-    public function setCouleurCasquette ($couleur) {
+    public function setCouleurCasquette($couleur)
+    {
         $this->_couleurCasquette = $couleur;
     }
 
-    public function getClasse () {
+    public function getClasse()
+    {
         return $this->_classe;
     }
 
-    public function getInfo () {
-        return "<p>".$this->_nom." à une force de " .$this->_force. ". Il a la classe " .$this->_classe. " et a une casquette de couleur " .$this->_couleurCasquette.".</p>";
+    public function getInfo()
+    {
+        return '<h4 class="text-center m-5">' . $this->_nom . ' à une force définie aléatoirement. Il a la classe ' . $this->_classe . ' et a une casquette de couleur ' . $this->_couleurCasquette . '.</h4>';
     }
 
-    public function frapper (Personnage $personnage) {
+    public function frapper(Personnage $personnage)
+    {
+        echo "<p class='text-center'>". "<span class='text-warning'>" .$this->_nom. "</span>"." va frapper ". $personnage->_nom. ".</p>";
+        $this->_force = rand(1, 50);
         return $personnage->recevoirDegat($this->_force);
     }
-    public function recevoirDegat ($force) {
+    public function recevoirDegat($force)
+    {
         $this->_vie = $this->_vie - $force;
 
-        if($this->_vie <=0) {
-            echo "<p>" .$this->_nom." a perdu " .$force. " points de vie. Il a succombé à ses blessures";
-        }else {
-            echo "<p>" .$this->_nom." a perdu " .$force. " points de vie. Il lui reste ".$this->_vie." points.</p>";
+        if ($this->_vie <= 0) {
+            echo '<p class="text-center">' . $this->_nom . ' a perdu ' . $force . ' points de vie. Il a succombé à ses blessures </p>';
+        } else {
+            echo '<p class="text-center">' . $this->_nom . ' a perdu ' . $force . ' points de vie. Il lui reste ' . $this->_vie . ' points.</p>';
         }
     }
 
+    public function getVie()
+    {
+        return $this->_vie;
+    }
 }
 
 // on crée une instance de notre classe personnage, on crée un objet de notre classe personnage
-$mario = new Personnage("Mario", 45, "rouge");
-$luigi = new Personnage("Luigi", 40, "verte");
+$mario = new Personnage('Mario', 45, 'rouge');
+$luigi = new Personnage('Luigi', 40, 'verte');
 
 echo $mario->getInfo();
 echo $luigi->getInfo();
-$mario->frapper($luigi);
-$luigi->frapper($mario);
-$mario->frapper($luigi);
-$luigi->frapper($mario);
-$mario->frapper($luigi);
+
+do {
+    $mario->frapper($luigi);
+    $luigi->frapper($mario);
+} while ($mario->getVie() > 0 && $luigi->getVie() > 0);
+
 
 // modifier la force de Mario
 // $mario->setForce(10);
